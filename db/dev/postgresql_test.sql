6b181001ea82f15d3e1354654910b16dc81b8f70
foxyo <>
2023-08-28 17:11:11 +0800

    feat-testing-sql-review-16

--- db/dev/postgresql_test.sql
+++ db/dev/postgresql_test.sql
@@ -2,4 +2,3 @@ CREATE TABLE t1 (
     id INTEGER,
     a_very_loooooooooong_column_name TEXT NOT NULL DEFAULT ''
 );
-DELETE FROM open_my_book2;
--- db/dev/schema_ch.sql
+++ db/dev/schema_ch.sql
@@ -1,1 +1,2 @@
 ALTER TABLE project ADD schema_version_type TEXT NOT NULL CHECK (schema_version_type IN ('TIMESTAMP', 'SEMANTIC')) DEFAULT 'TIMESTAMP';
+DELETE FROM open_my_book2;
